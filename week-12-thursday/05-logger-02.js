5aa2703f1f429ad32f6d3e30aef3c1900fef6ba1
Adetoye Babalola <adetoyebamise@gmail.com>
2021-05-04 16:34:05 +0100

    logger application preimp;ementation

---
+++ week-12-thursday/05-logger-02.js
@@ -0,0 +1,27 @@
+const eventEmitter = require("events");
+const loggerEmitter = new eventEmitter();
+// function logActivity( ) {
+//   process.stdout.write()
+// }
+// logActivity();
+// loggerEmitter.on("event", () => {
+// process.stdout.write("message", (data) => {
+// ` Please Drop your message : \n ${data.toString().trim()}`;
+// });
+// console.log(`Scan your fingerprint, You are logged in at : ${new Date()}`);
+// });
+// process.stdout.on("data", (data) => {
+//   process.stdout.write(` Please Drop your message : \n ${data}`);
+//   process.exit();
+// });
+// loggerEmitter.emit("event");
+
+// const event = loggerEmitter
+//   .getDefaultCalendar()
+//   .createAllDayEvent("Apollo 11 Landing", new Date("July 20, 1969"), {
+//     location: "The Moon",
+//   });
+// Logger.log("Event ID: " + event.getId());
+function createAllDayEvent(title, date) {
+  console.log(`Evening Node classes, ${new Time()}`);
+}
--- week-12-thursday/05-logger.js
+++ week-12-thursday/05-logger.js
@@ -1,9 +1,11 @@
-const EventEmitter = require("events");
-const eventEmitter = new EventEmitter();
-eventEmitter.on("event", (message, date) => {
-  process.stdin();
+const eventEmitter = require("events");
+const loggerEmitter = new eventEmitter();
+loggerEmitter.on("event", () => {
+  console.log(
+    `You scanned your fingerprint, You are logged in at : ${new Date()}`
+  );
 });
-eventEmitter.emit("event", `This is the time on the West coast:`, new Date());
+
+loggerEmitter.emit("event");
 
 // A device or computer program for making a systematic recording of events, observations, or measurements.
-("a data logger");
