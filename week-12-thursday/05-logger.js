eb91585d09f53c81f4d2394e48815f9602db2b34
Adetoye Babalola <adetoyebamise@gmail.com>
2021-05-04 18:06:10 +0100

    logger attempts: work way around it before class

--- week-12-thursday/05-logger.js
+++ week-12-thursday/05-logger.js
@@ -1,11 +1,16 @@
 const eventEmitter = require("events");
 const loggerEmitter = new eventEmitter();
-loggerEmitter.on("event", () => {
+const logger = (message) => {
+  loggerEmitter.emit("event", `${message}`);
+};
+
+loggerEmitter.on("event", (message) => {
   console.log(
-    `You scanned your fingerprint, You are logged in at : ${new Date()}`
+    `${message} scanned your fingerprint, You are logged in at : ${new Date()}`
   );
 });
 
-loggerEmitter.emit("event");
+logger("Kee");
 
+exports.logger = logger;
 // A device or computer program for making a systematic recording of events, observations, or measurements.
