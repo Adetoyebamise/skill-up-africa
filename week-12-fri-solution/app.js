20959da4918e65ffb444a8dea1d47f4547068e79
Adetoye Babalola <adetoyebamise@gmail.com>
2021-05-06 12:52:42 +0100

    changes: refactor the video streaming code

--- week-12-fri-solution/app.js
+++ week-12-fri-solution/app.js
@@ -1,13 +1,13 @@
 const http = require("http");
 const fs = require("fs");
-const Stream = require("stream");
-const writableStream = new Stream.Writable();
+const path = require("path");
 
 const server = http.createServer((request, response) => {
   response.writeHead(200, { "Content-Type": "video/mp4" });
-  const stream = fs.createReadStream(__dirname + "/1-fav-scene-recording.mp4");
-  let body = "";
-  stream.pipe(response);
+  const readableStream = fs.createReadStream(
+    __dirname + "/1-fav-scene-recording.mp4"
+  );
+  readableStream.pipe(response);
 });
 
 server.listen(3000, () => {
